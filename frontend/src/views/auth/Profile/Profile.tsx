b96b5c8575fd9e24e24f760e44510bafa184c90e
Kim Andersson <kandersson@gmail.com>
2025-06-11 15:06:55 +0200

    removed useEffect from import

--- frontend/src/views/auth/Profile/Profile.tsx
+++ frontend/src/views/auth/Profile/Profile.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useState } from "react";
 import { UserContext } from "../../../UserContext";
 import { Link } from "react-router-dom";
 import Popup from "../../../components/Popup/Popup";
